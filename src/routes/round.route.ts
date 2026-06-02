ee32cbb7b1cafbdc395d459bb6d15377fee3d1b7
CaioCesarRocha <caio.cesar209@gmail.com>
2026-06-02 13:42:46 -0300

    feat: create endpoint to update round, remove mandatory identifier field from get rounds

--- src/routes/round.route.ts
+++ src/routes/round.route.ts
@@ -3,16 +3,24 @@ import { Router } from "express";
 import { RoundController } from "../controllers/round.controller";
 import { validateRequest } from "../middlewares/validate-request";
 import { RoundService } from "../services/round.service";
-import { createRoundBodySchema, listRoundsByFilterParamsSchema } from "./round.schema";
+import { createRoundBodySchema, listRoundsByFilterParamsSchema, listRoundsByFilterQuerySchema, updateRoundBodySchema, updateRoundParamsSchema } from "./round.schema";
 
 const router = Router();
 const roundService = new RoundService();
 const roundController = new RoundController(roundService);
 
 router.post("/", validateRequest({ body: createRoundBodySchema }), roundController.createRound);
+router.put(
+  "/:id",
+  validateRequest({ body: updateRoundBodySchema, params: updateRoundParamsSchema }),
+  roundController.updateRound,
+);
 router.get(
-  "/:championshipId/:identifier",
-  validateRequest({ params: listRoundsByFilterParamsSchema }),
+  "/:championshipId",
+  validateRequest({
+    params: listRoundsByFilterParamsSchema,
+    query: listRoundsByFilterQuerySchema,
+  }),
   roundController.listRoundsByFilter,
 );
 
